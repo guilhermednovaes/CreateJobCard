51f03a8ed798cc9cf9987d0588b8299893c9c51a
guilhermednovaes <167258906+guilhermednovaes@users.noreply.github.com>
2024-07-30 15:40:39 -0300

    Update JobCard.py

--- JobCard.py
+++ JobCard.py
@@ -132,7 +132,7 @@ def generate_template(jc_number, issue_date, area, spools, sgs_df):
     worksheet.merge_range(f'F{row+5}', 'CC', merge_format)
     worksheet.merge_range(f'G{row+5}:L{row+5}', '', merge_format)
     
-    worksheet.merge_range(f'A{row+5}:E{row6}', '', merge_format)
+    worksheet.merge_range(f'A{row+5}:E{row+6}', '', merge_format)
     worksheet.merge_range(f'F{row+6}', '', merge_format)
     worksheet.merge_range(f'G{row+6}:L{row+6}', '', merge_format)
 
