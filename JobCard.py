0311bb890bd2b8b67b8e96082c9529c45336202d
guilhermednovaes <167258906+guilhermednovaes@users.noreply.github.com>
2024-07-30 13:48:11 -0300

    Update JobCard.py

--- JobCard.py
+++ JobCard.py
@@ -53,24 +53,29 @@ def generate_template(jc_number, issue_date, area, spools, sgs_df):
     merge_format, header_format, cell_format = create_formats(workbook)
 
     worksheet.set_column('A:L', 15)
-    worksheet.merge_range('C1:D1', 'PETROBRAS', merge_format)
+    worksheet.set_row(0, 30)
+    worksheet.set_row(1, 30)
+    worksheet.set_row(2, 30)
+    
+    worksheet.merge_range('B1:C1', 'PETROBRAS', merge_format)
     worksheet.merge_range('E1:F1', 'FPSO_P-82', merge_format)
     worksheet.merge_range('G1:H1', 'Request For Fabrication', merge_format)
     
-    worksheet.insert_image('A1', 'Logo/BR.png', {'x_offset': 10, 'y_offset': 5, 'x_scale': 0.5, 'y_scale': 0.5})
-    worksheet.insert_image('I1', 'Logo/Seatrium.png', {'x_offset': 10, 'y_offset': 5, 'x_scale': 0.5, 'y_scale': 0.5})
+    # Inserção das Imagens
+    worksheet.insert_image('B1', 'Logo/BR.png', {'x_offset': 15, 'y_offset': 5, 'x_scale': 0.5, 'y_scale': 0.5})
+    worksheet.insert_image('G1', 'Logo/Seatrium.png', {'x_offset': 15, 'y_offset': 5, 'x_scale': 0.5, 'y_scale': 0.5})
     
-    worksheet.merge_range('A4:H4', f'JC Number : {jc_number}', merge_format)
+    worksheet.merge_range('B4:H4', f'JC Number : {jc_number}', merge_format)
+    worksheet.merge_range('B5:H5', f'Issue Date : {issue_date}', merge_format)
     worksheet.merge_range('I4:L4', area, merge_format)
-    worksheet.merge_range('A5:H5', f'Issue Date : {issue_date}', merge_format)
     
-    worksheet.merge_range('A6:L6', 'Special Instruction : Please be informed that Materials for the following. SPOOL PIECE No.[s] are available for Issuance.', merge_format)
+    worksheet.merge_range('B6:L6', 'Special Instruction : Please be informed that Materials for the following. SPOOL PIECE No.[s] are available for Issuance.', merge_format)
     
     headers = ['No.', 'Area / WBS', 'Spool', 'Sheet', 'Size', 'Paint Code', 'REV.', 'Shop ID', 'Weight', 'Base Material', 'Material Status', 'Remarks']
-    worksheet.write_row('A7', headers, header_format)
+    worksheet.write_row('B7', headers, header_format)
     
     row = 7
-    col = 0
+    col = 1
     total_weight = 0
     spools_list = list(dict.fromkeys([spool.strip() for spool in spools.split('\n') if spool.strip()]))
     for idx, spool in enumerate(spools_list):
@@ -93,18 +98,18 @@ def generate_template(jc_number, issue_date, area, spools, sgs_df):
         worksheet.write_row(row, col, data, cell_format)
         row += 1
     
-    worksheet.merge_range(f'A{row+1}:B{row+1}', 'Total Weight: (Kg)', merge_format)
-    worksheet.write(f'C{row+1}', total_weight, merge_format)
+    worksheet.merge_range(f'B{row+1}:C{row+1}', 'Total Weight: (Kg)', merge_format)
+    worksheet.write(f'D{row+1}', total_weight, merge_format)
     
-    worksheet.merge_range(f'A{row+2}:B{row+2}', 'Prepared by', merge_format)
-    worksheet.merge_range(f'C{row+2}:D{row+2}', 'Approved by', merge_format)
-    worksheet.merge_range(f'E{row+2}:F{row+2}', 'Received', merge_format)
+    worksheet.merge_range(f'B{row+2}:C{row+2}', 'Prepared by', merge_format)
+    worksheet.merge_range(f'D{row+2}:E{row+2}', 'Approved by', merge_format)
+    worksheet.merge_range(f'F{row+2}:G{row+2}', 'Received', merge_format)
     
-    worksheet.merge_range(f'A{row+3}:B{row+3}', 'Piping Engg.', merge_format)
-    worksheet.merge_range(f'C{row+3}:D{row+3}', 'J/C Co-Ordinator', merge_format)
-    worksheet.merge_range(f'E{row+3}:F{row+3}', 'Spooling Vendor : EJA', merge_format)
+    worksheet.merge_range(f'B{row+3}:C{row+3}', 'Piping Engg.', merge_format)
+    worksheet.merge_range(f'D{row+3}:E{row+3}', 'J/C Co-Ordinator', merge_format)
+    worksheet.merge_range(f'F{row+3}:G{row+3}', 'Spooling Vendor : EJA', merge_format)
     
-    worksheet.merge_range(f'A{row+4}:F{row+4}', 'CC', merge_format)
+    worksheet.merge_range(f'B{row+4}:G{row+4}', 'CC', merge_format)
     
     workbook.close()
     output.seek(0)
